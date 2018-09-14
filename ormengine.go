ff40bae339a522fcea1cc60c2088701e21091fe3
weit <tanwei.rush@gmail.com>
2018-09-14 16:27:50 +0800

    1. doc 2. 3. 4.

--- ormengine.go
+++ ormengine.go
@@ -18,6 +18,9 @@ func newXorm(cfg config.OrmEngineConfig) (*xengine.Orm, error) {
 	return xengine.NewOrm(cfg)
 }
 
+// NewOrm 创建一个Orm
+// t: Orm类型，有gorm、xorm两种
+// cfg: orm的配置
 func NewOrm(t string, cfg config.OrmEngineConfig) (interface{}, error) {
 	switch t {
 	case "gorm":
